a776609a18daebf153de6b185a16a104e87ec7e8
Ibrahim <halidu.m1602411@st.futminna.edu.ng>
2025-02-22 22:57:32 -0800

    0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,5 @@
 #!/usr/bin/node
-// script that reads and prints the content of a file
-// const fs = require('fs');
-// const myArgs = process.argv.slice(2);
-// fs.readFile(myArgs[0], 'utf-8', function (err, data) {
-//   if (err) {
-//       console.log(err);
-//         } else {
-//             console.log(data);
-//               }
-//               });
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
